6f11e5084d6e7a85576d36f495198464803ab031
luocoding <luxul233@foxmail.com>
2017-03-25 10:22:12 +0800

    2048

---
+++ 2048/index.js
@@ -0,0 +1,162 @@
+// 初始数组
+function arr() {
+	var span=document.getElementsByTagName("span")[0].innerText=0;
+	var ar=new Array();
+	ar[0]=[0,0,0,0];
+	ar[1]=[0,0,0,0];
+	ar[2]=[0,0,0,0];
+	ar[3]=[0,0,0,0];
+    //绑定到各个表格
+    var td=document.getElementsByTagName("td");
+    for (var i=0;i<4;i++){
+    	for(var j=0;j<4;j++){
+    		td[4*i+j].innerText=ar[i][j];
+    		td[4*i+j].style.color="#CCC0B2";
+
+    	}
+
+    }
+    
+}
+// 随机出现2
+function random2() {
+	var td=document.getElementsByTagName("td");
+	var ary=[];
+	for(var i=0;i<16;i++){
+		if(td[i].innerText==0){//检查值是否为零
+			ary.push(i);
+		}
+	}
+	if(ary.length!==0){
+		var num=Math.floor((Math.random()*ary.length));
+		var nu=Math.floor((Math.random()*100+1));
+		var h=ary[num];
+		if(nu>=40){
+			td[h].innerText=2;
+			td[h].style.backgroundColor="#EEE4DA"
+		}
+		if(nu<40){
+			td[h].innerText=4;
+			td[h].style.backgroundColor="#ECE0C8"
+		}
+		
+		td[h].style.color="#7C736A";
+	}
+	
+
+}
+//检查游戏是否结束
+function checkOff(){
+	var td=document.getElementsByTagName("td");
+	for(var i=0;i<td.length;i++){
+		if(td[i].innerText==2048){
+			var span=document.getElementsByTagName("span")[0].innerText;
+			alert("你赢了，你的分数是："+span)
+			break;
+		}
+	}
+	var a=getarr("up").toString();
+	var len=getarr("up").length;
+	var b=getarr("down").toString();
+	var c=getarr("left").toString();
+	var d=getarr("right").toString();
+	var aa=controlMove(getarr("up")).toString();
+	var bb=controlMove(getarr("down")).toString();
+	var cc=controlMove(getarr("left")).toString();
+	var dd=controlMove(getarr("right")).toString();
+	
+	if((a==aa)&&(b==bb)&&(c==cc)&&(d==dd)){
+		if(confirm("游戏结束，是否重玩？")){  
+	        arr();
+	        random2();
+	        move();
+	        changeColor();  
+            }else{
+            	arr();
+	            random2();
+	            move();
+	            changeColor();
+            }
+	}
+
+}
+// 操作
+function move() {
+	document.onkeydown=function (event){
+		var e=event||window.event;
+		var key=e&&e.keyCode;
+		if(key==38) {//上
+			var a=getarr("up");
+            var aa=controlMove(a,"ischeck");
+            	
+            addValue(aa,"up");
+            checkOff();
+            countScore();	
+			random2();
+			changeColor();
+		}
+		if (key==40) {//下
+			var b=getarr("down");
+            var bb=controlMove(b,"ischeck");	
+            addValue(bb,"down");
+            checkOff();
+            countScore();	
+			random2();
+			changeColor();
+
+		}
+		if (key==37) {//左
+			var c=getarr("left");
+            var cc=controlMove(c,"ischeck");	
+            addValue(cc,"left");
+            checkOff();
+            countScore();	
+			random2();
+			changeColor();
+
+		}
+		if (key==39) {//右
+			var d=getarr("right");
+            var dd=controlMove(d,"ischeck");	
+            addValue(dd,"right");
+            checkOff();
+            countScore();	
+			random2();
+			changeColor();
+
+		}
+	}
+
+}
+// 计分
+function countScore() {
+	var ii=0;
+	for(var i=0;i<score.length;i++){
+		ii=ii+score[i];
+	}
+	var span=document.getElementsByTagName("span")[0];
+	var iii=parseInt(span.innerText);//字符串转为数字
+	span.innerText=ii+iii;
+	score=[];
+
+
+}
+//初始化函数
+function init(){
+	// 初始数组
+	arr();
+	// 随机出现2
+	random2();
+	random2();
+	// 操作
+	move();
+	// 改变颜色
+	changeColor();
+	
+}
+//初始化
+var score=[];
+
+init();
+
+
---
+++ 2048/support.js
@@ -0,0 +1,221 @@
+//获取值存入数组
+function getarr(asx){
+	var a=new Array(4);
+	for(var i=0;i<a.length;i++){
+		a[i]=new Array(4);
+	}
+	var td=document.getElementsByTagName("td");
+	if(asx=="up"){
+		for(var j=0;j<a.length;j++){
+			a[j][0]=td[0+j].innerText;
+			a[j][1]=td[4+j].innerText;
+			a[j][2]=td[8+j].innerText;
+			a[j][3]=td[12+j].innerText;
+		}
+		return a;
+	}
+	if(asx=="down"){
+		for(var k=0;k<a.length;k++){
+			a[k][0]=td[12+k].innerText;
+			a[k][1]=td[8+k].innerText;
+			a[k][2]=td[4+k].innerText;
+			a[k][3]=td[0+k].innerText;
+		}
+		return a;
+	}
+	if(asx=="left"){
+		for(var h=0;h<a.length;h++){
+			a[h][0]=td[0+4*h].innerText;
+			a[h][1]=td[1+4*h].innerText;
+			a[h][2]=td[2+4*h].innerText;
+			a[h][3]=td[3+4*h].innerText;
+		}
+		return a;
+	}
+	if(asx=="right"){
+		for(var p=0;p<a.length;p++){
+			a[p][0]=td[3+4*p].innerText;
+			a[p][1]=td[2+4*p].innerText;
+			a[p][2]=td[1+4*p].innerText;
+			a[p][3]=td[0+4*p].innerText;
+		}
+		return a;
+	}
+    
+	
+} 
+
+//控制操作
+function controlMove(a,ischeck){
+	var len=a.length;
+	var l=len;
+	for(var i=0;i<len;i++){
+		a.push(addSum(a[i],ischeck));
+	}
+	return a.slice(l);        
+}
+//合并算法
+/*function addSum(arro){
+	
+	var len=arro.length;
+	var m=0,n=0;
+	for(;n<len;n++){
+	for(var i=0;i<len;i++) {
+		m=i+1;
+		if(i!==3){
+		if(arro[i]==0&&arro[m]!==0){arro[i]=arro[m];arro[m]=0;continue;}
+		if(arro[i]==0&&arro[m]==0){continue;}
+		if(arro[i]!==0&&arro[m]==0){continue;}
+		if(arro[i]!==0&&arro[m]!==0){
+			if(arro[i]==arro[m]){
+				arro[i]=2*arro[m];
+				arro[m]=0;
+				continue;
+			}else{
+				continue;
+			}
+		}
+	}else{break;}
+    }
+}
+
+
+    return arro;
+
+}*/
+//合并算法
+function addSum(arro,ischeck){
+	
+	var len=arro.length;
+	var m=0,n=0;
+	var mm=[1,1,1,1];
+	for(;n<len;n++){
+	for(var i=0;i<len;i++) {
+		m=i+1;
+		if(i!==3){
+		if(arro[i]==0&&arro[m]!==0){arro[i]=arro[m];arro[m]=0;continue;}
+		if(arro[i]==0&&arro[m]==0){continue;}
+		if(arro[i]!==0&&arro[m]==0){continue;}
+		if(arro[i]!==0&&arro[m]!==0){
+			if(arro[i]==arro[m]){
+				if((mm[i]==1)&&(mm[m]==1)){
+				arro[i]=2*arro[m];
+				if(ischeck==="ischeck"){
+					score.push(arro[i]);//记分
+				}
+				arro[m]=0;
+				mm[i]=2;
+				break;}else{break;}
+			}else{
+				continue;
+			}
+		}
+	}else{break;}
+    }
+}
+
+
+    return arro;
+
+}
+
+
+//值为零时将颜色改变
+function changeColor(){
+	var td=document.getElementsByTagName("td");
+	for(var i=0;i<td.length;i++){
+		var h=td[i].innerText;
+		switch(h){
+			case "0":
+			td[i].style.color="#CCC0B2";
+			td[i].style.backgroundColor="#CCC0B2";
+			break;
+			case "2":
+			td[i].style.color="#7C736A";
+			td[i].style.backgroundColor="#EEE4DA";
+			break;
+			case "4":
+			td[i].style.color="#7C736A";
+			td[i].style.backgroundColor="#ECE0C8";
+			break;
+			case "8":
+			td[i].style.color="#FFF7EB";
+			td[i].style.backgroundColor="#F2B179";
+			break;
+			case "16":
+			td[i].style.color="#FFF7EB";
+			td[i].style.backgroundColor="#F59563";
+			break;
+			case "32":
+			td[i].style.color="#FFF7EB";
+			td[i].style.backgroundColor="#F57C5F";
+			break;
+			case "64":
+			td[i].style.color="#FFF7EB";
+			td[i].style.backgroundColor="#F65D3B";
+			break;
+			case "128":
+			td[i].style.color="#FFF7EB";
+			td[i].style.backgroundColor="#F6D575";
+			break;
+			case "256":
+			td[i].style.color="#FFF7EB";
+			td[i].style.backgroundColor="#EDCC61";
+			break;
+			case "512":
+			td[i].style.color="#FFF7EB";
+			td[i].style.backgroundColor="#00A1E1";
+			break;
+			case "1024":
+			td[i].style.color="#FFF7EB";
+			td[i].style.backgroundColor="#8EEB1E";
+			break;
+			case "2048":
+			td[i].style.color="#FFF7EB";
+			td[i].style.backgroundColor="#EFED1C";
+			break;
+
+		}
+
+
+	}
+
+}
+//将数组的值绑定到表格
+function addValue (a,asx){
+	var td=document.getElementsByTagName("td");
+	if(asx=="up"){
+		for(var j=0;j<a.length;j++){
+			td[0+j].innerText=a[j][0];
+			td[4+j].innerText=a[j][1];
+			td[8+j].innerText=a[j][2];
+			td[12+j].innerText=a[j][3];
+		}
+	}
+	if(asx=="down"){
+		for(var k=0;k<a.length;k++){
+			td[12+k].innerText=a[k][0];
+			td[8+k].innerText=a[k][1];
+			td[4+k].innerText=a[k][2];
+			td[0+k].innerText=a[k][3];
+		}
+	}
+	if(asx=="left"){
+		for(var h=0;h<a.length;h++){
+			td[0+4*h].innerText=a[h][0];
+			td[1+4*h].innerText=a[h][1];
+			td[2+4*h].innerText=a[h][2];
+			td[3+4*h].innerText=a[h][3];
+		}
+	}
+	if(asx=="right"){
+		for(var p=0;p<a.length;p++){
+			td[3+4*p].innerText=a[p][0];
+			td[2+4*p].innerText=a[p][1];
+			td[1+4*p].innerText=a[p][2];
+			td[0+4*p].innerText=a[p][3];
+		}
+	}
+
+
+}
